14257da3323f82b1d236f066fe11a8dbf6e6c17f
RafaelKC <rafaelkaua97@gmail.com>
2023-08-22 18:59:04 -0300

    Ajuste do thankyou

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -4,24 +4,38 @@ import github from '../assets/github.png'
 import telegram from '../assets/telegram.png'
 
 import { i18n } from '../translates/i18n'
+import { useState } from 'react'
 
 export function Contact() {
+
+    const [sended, setSended] = useState(false);
+
     return (
         <div id="contact" className='min-h-[800px] md:min-h-[450px] lg:min-h-[900px] flex flex-col justify-center items-center'>
             <div className="flex flex-col items-center gap-8 w-full">
                 <h1 className="text-4xl font-bold">{i18n.t('contacts.title')}</h1>
                 <div className="w-11/12 lg:w-1/3">
+
+                     {sended ? 
+                        <h1 className="font-bold text-xl lg:text-4xl">{i18n.t('contacts.thankYou')}</h1>
+                     :
+
                     <form className="flex flex-col items-start gap-3 w-full" action="https://api.staticforms.xyz/submit" method="post">
                         <input type="hidden" name="accessKey" value="e8957dc5-2573-4cec-b37b-908cb2dbe489"></input>
-                        <input type="hidden" name="redirectTo" value="http://45.79.198.12/thank-you"></input>
 
                         <input className="w-full rounded-xl p-1 border-zinc-900 border-2" type="text" name="name" required placeholder={i18n.t('contacts.name')}></input>
                         <input className="w-full rounded-xl p-1 border-zinc-900 border-2" type="email" name="email" required placeholder={i18n.t('contacts.email')}></input>
                         <input className="w-full rounded-xl p-1 border-zinc-900 border-2" type="text" name="$company" placeholder={i18n.t('contacts.company')}></input>
                         <textarea className="w-full rounded-xl p-1 border-zinc-900 border-2 max-h-52" name="message" required placeholder={i18n.t('contacts.message')}></textarea>
 
-                        <input className="w-1/3 rounded-xl p-1 bg-slate-200 border-zinc-900 border-2 cursor-pointer hover:bg-slate-400" type="submit" value={i18n.t('contacts.send')}/>
-                    </form>
+                        <input
+                            className="w-1/3 rounded-xl p-1 bg-slate-200 border-zinc-900 border-2 cursor-pointer hover:bg-slate-400"
+                            type="submit"
+                            value={i18n.t('contacts.send')} 
+                            onClick={() => { setSended(true) }}
+                            />
+                    </form> 
+                    }
                 </div>
             </div>
 
