d4bafecfc30450eff2aa68989527771b7a372d41
RafaelKC <rafaelkaua97@gmail.com>
2023-08-22 20:37:48 -0300

    Adding more translations

--- src/translates/translations/en.i18n.ts
+++ src/translates/translations/en.i18n.ts
@@ -13,13 +13,13 @@ const messages = {
                     title: 'Web FullStack Developer',
                     date: 'June 2021 - currently',
                     obs: '13* months of total experience',
-                    more: 'moremore'
+                    more: 'Frontend development with Angular and backend with C#/Entity Framework. I develop business management and logistics applications. Contact with Docker/Docker Compose/Portainer, Consul, SQL Server, PostgreSQL, GO and database migration.'
                 },
                 pucpr: {
                     title: 'Software Engineering',
                     date: 'April 2023 - currently',
                     obs: '7 semesters to complete',
-                    more: 'moremore'
+                    more: 'Currently Bachelor\'s Degree in Software Engineering at Pontifical Catholic University of Paraná, in Curitiba/Brazil, expected to be completed in November 2026.In the course I have contact with several programming languages, paradigms and best practices. But I also have contact with the soft parts of programming, such as modeling flows and databases, requirements, team management and development models.'
                 }
             },
             languages: {
--- src/translates/translations/pt.i18n.ts
+++ src/translates/translations/pt.i18n.ts
@@ -13,13 +13,13 @@ const messages = {
                     title: 'Desenvolvedor Web FullStack',
                     date: 'Jun 2021 - atual',
                     obs: '13* meses de experiência total',
-                    more: 'moremore'
+                    more: 'Desenvolvimento de frontend com Angular e backend com C#/Entity Framework. Desenvolvo aplicativos de gestão e logística empresarial. Contato com Docker/Docker Compose/Portainer, Consul, SQL Server, PostgreSQL, GO e migração de bando de dados.'
                 },
                 pucpr: {
                     title: 'Engenharia de Software',
                     date: 'Abril 2023 - atual',
                     obs: '7 semestres para conclusão',
-                    more: 'moremore'
+                    more: 'Curso atualmente Bacharelado em Engenharia de Software pela Pontifícia Universidade Católica do Paraná, em Curitiba/Brasil, com previsão para conclusão em novembro de 2026.No curso tenho contato com diversas linguagens de programação, paradigmas e boas práticas. Mas também tenho contato com a parte soft da programação, como modelagem de fluxos e bancos de dados, levantamento de requisitos, gestão de equipes, modelos de desenvolvimento e também um pouco de contato com o cyber físico.'
                 }
             },
             languages: {
